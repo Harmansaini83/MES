6ed846dd149e7a4a1e083119e2d049a48bc09347
Harmansaini83 <harmanajitsinghsaini16@gmail.com>
2026-02-05 00:11:42 +0530

    Update Q1.asm

--- Q1.asm
+++ Q1.asm
@@ -1,14 +1,20 @@
 ORG 0000H
 
-MOV A, #01H
-MOV B, #0AH
-MUL AB
-ADD A, #00H
-MOV B, #0AH
-MUL AB
-ADD A, #01H
-MOV B, #0AH
-MUL AB
-ADD A, #01H
+MOV A, #01H        ; A = 1
+MOV B, #0AH        ; B = 10
+MUL AB             ; A = 1 × 10 = 10
 
-HERE: SJMP HERE
+ADD A, #00H        ; A = 10
+MOV B, #0AH
+MUL AB             ; A = 10 × 10 = 100
+
+ADD A, #05H        ; A = 105
+MOV B, #0AH
+MUL AB             ; A = 105 × 10 = 1050
+
+ADD A, #06H        ; A = 1056 (Final result)
+
+HERE: SJMP HERE    ; Stop execution
+
+END
+
