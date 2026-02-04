f24d2717903b6d38a76d103e5c2a5e352e4c93af
Harmansaini83 <harmanajitsinghsaini16@gmail.com>
2026-02-05 00:20:37 +0530

    Update Q4.asm

--- Q4.asm
+++ Q4.asm
@@ -1,10 +1,21 @@
 ORG 0000H
 
+; ----- Construct lower byte (6FH) -----
+CLR A
+ORL A, #60H
+ORL A, #0FH
+MOV R0, A
+
+; ----- Construct upper byte (25H) -----
 CLR A
 ORL A, #20H
 ORL A, #05H
-MOV B, A
 
-CLR A
-ORL A, #20H
-ORL A, #04H
+; ----- Combine upper and lower bytes logically -----
+SWAP A
+ANL A, #0F0H
+SWAP A
+ORL A, R0
+
+HERE: SJMP HERE
+END
